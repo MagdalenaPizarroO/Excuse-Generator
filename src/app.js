fc57de1d958cffc92884a97dc0c3d60d5e97d981
MagdalenaPizarroO <mspizarro.ortega@gmail.com>
2023-02-18 00:24:53 +0000

    ya casi

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./style.css";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  //Math.floor para que me incluya el valor 0, que es el primer elemento de cada arreglo
+  let randomWhen = Math.floor(Math.random() * when.length); // lo multiplico por el número de elementos del arreglo;
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+
+  let resultado =
+    who[randomWho] +
+    " " +
+    action[randomAction] +
+    " " +
+    what[randomWhat] +
+    " " +
+    when[randomWhen];
+  console.log(resultado);
 };
